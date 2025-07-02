5da2a11bc167e96549a549e8acc1c9badc28a867
Kaletsidik Ayalew <alexkalalw@gmail.com>
2025-07-02 13:44:37 +0300

    establish the single source of trust, the appState

---
+++ coreState.js
@@ -0,0 +1,15 @@
+const appState = {
+  balances: {
+    ETB: 1000,
+    USD: 200,
+    EUR: 100
+  },
+  transactionHistory: [],
+  preferences: {
+    primaryCurrency: "ETB"
+  },
+  historyStack: [],
+  futureStack: []
+};
+
+module.exports = { appState };
