d7880ebf99bd2cfce964f53a78f598ac95189796
Kaletsidik Ayalew <alexkalalw@gmail.com>
2025-07-03 07:46:51 +0300

    fix: the logger to return the function that performs logging

--- logger.js
+++ logger.js
@@ -1,6 +1,9 @@
 const logTransaction = (type) => (currency) => (amount) => (description = "") => () => {
   const timestamp = new Date().toISOString();
+
   console.log(`[${timestamp}] ${type}: ${amount} ${currency} — ${description}`);
+  
+  return logTransaction;
 };
 
 module.exports = { logTransaction };
